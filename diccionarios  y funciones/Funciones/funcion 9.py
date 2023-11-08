f83c6a39c28444417f6f8ff56d7a4ee3557f3915
Meli-Lopez <129072033+Meli-Lopez@users.noreply.github.com>
2023-11-08 11:51:28 -0500

    Update funcion 9.py

--- diccionarios  y funciones/Funciones/funcion 9.py
+++ diccionarios  y funciones/Funciones/funcion 9.py
@@ -1,7 +1,7 @@
 #Funciones anonimas «lambda», con la palabra lambda no se necesita colocar el def
-numero_palabras = lambda t: len(t.strip().split()) # La t es el argumento que toma la funcion lambda
+numeroPalabras = lambda t: len(t.strip().split()) # La t es el argumento que toma la funcion lambda
 # len() se utiliza para calcula la longitud de la lista
 # strip() se utiliza para eliminar los espacios en blanco 
 # split() se utiliza para dividir la cadena en palabras separadas por espacios en blanco.
 # Llama a la funcion lambda con un argumento y lo imprime
-print(numero_palabras("hola, esto es una prueba con lambda")) 
+print(numeroPalabras("hola, esto es una prueba con lambda")) 
