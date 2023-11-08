28d02cbe078b70c79561493d1c69a2f853625d0f
Meli-Lopez <129072033+Meli-Lopez@users.noreply.github.com>
2023-11-08 11:49:37 -0500

    Update funcion 10.py

--- diccionarios  y funciones/Funciones/funcion 10.py
+++ diccionarios  y funciones/Funciones/funcion 10.py
@@ -1,10 +1,10 @@
 #Ejemplo 2 funcion Lambda, es una funcion de una sola sentencia y hace un operacion o actividad en particular 
 
 # Define una funcion (lambda) llamada "operadorand" que toma dos argumentos, "x" y "y" y realiza una operación AND en ellos
-operadorand = lambda x, y: x & y
+operadorAnd = lambda x, y: x & y
 # Inicia un bucle externo para i en el rango de 0 a 1, es decir que el bucle realiza 2 iteraciones
 for i in range(2):
     # Inicia un bucle interno para j en el rango de 0 a 1 (2 iteraciones) 
     for j in range(2):
         # Imprime una cadena que muestra los valores de i y j, asi como el resultado de la operacion AND
-        print(f"{i} & {j} = {operadorand(i, j)}") 
+        print(f"{i} & {j} = {operadorAnd(i, j)}") 
