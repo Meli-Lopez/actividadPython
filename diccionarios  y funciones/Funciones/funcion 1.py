46934d8538203d66675b4309e3b3fd3b9dac7aff
Meli-Lopez <129072033+Meli-Lopez@users.noreply.github.com>
2023-11-08 11:49:14 -0500

    Update funcion 1.py

--- diccionarios  y funciones/Funciones/funcion 1.py
+++ diccionarios  y funciones/Funciones/funcion 1.py
@@ -2,16 +2,16 @@
 def saludar():
     print("saludo") #El print se utliza dentro de la misma funcion y no altera el programa principal
 #Definir una funcion 
-def retornarnumero(): 
+def retornarNumero(): 
     return 1 # Devuelve el valor 1
 #devuelve la informacion por medio del programa principal y si puede afectar el programa principal
 
 # Se llama la funcion 'saludar' y la funcion 'retornarnumero'
 saludar()
-retornarnumero() 
+retornarNumero() 
 
 # Se compara el resultado de la funcion 'retornarnumero' con 1
-if retornarnumero()==1:
+if retornarNumero()==1:
     print("devolvio un uno")
 else:
     print("No devolvio un uno")
