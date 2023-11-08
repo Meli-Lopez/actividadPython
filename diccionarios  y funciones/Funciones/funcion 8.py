2bc2b9af879af5d5a0246cb6da4019438bf55fd0
Meli-Lopez <129072033+Meli-Lopez@users.noreply.github.com>
2023-11-08 11:51:10 -0500

    Update funcion 8.py

--- diccionarios  y funciones/Funciones/funcion 8.py
+++ diccionarios  y funciones/Funciones/funcion 8.py
@@ -1,6 +1,6 @@
 #Modificando parametros mutables
 # Define una funcion llamada "listalimpia" que toma dos argumentos = "arg" y "result"
-def listalimpia(arg, result=None): # "None" indica que si no se proporciona un valor al parametro cuando se llama a la funcion, se le asignara un valor predeterminado de "None"
+def listaLimpia(arg, result=None): # "None" indica que si no se proporciona un valor al parametro cuando se llama a la funcion, se le asignara un valor predeterminado de "None"
     # Verificar si "result" es None 
     if result is None:
         # Si "result" es None, se inicializa como una lista vacia
@@ -10,5 +10,5 @@ def listalimpia(arg, result=None): # "None" indica que si no se proporciona un v
     # Se imprime la lista
     print(result)
 # Llama la funcion "listalimpia" con el argumento "a" y "b"
-listalimpia("a")
-listalimpia("b") 
+listaLimpia("a")
+listaLimpia("b") 
