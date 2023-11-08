57c3a3de7c98fad4e9ba78b8675ea667e59dcad6
Meli-Lopez <129072033+Meli-Lopez@users.noreply.github.com>
2023-11-08 11:47:27 -0500

    Update 2 diccionario.py

--- diccionarios  y funciones/Diccionarios/2 diccionario.py
+++ diccionarios  y funciones/Diccionarios/2 diccionario.py
@@ -1,5 +1,5 @@
-Diccionario = {'nombre': 'Sandra', 'edad': 44}
-# Accediendo a los elementos de un diccionario: 
-print(Diccionario['nombre']) # Imprime el valor asociado a la clave 'nombre' en el diccionario.
-print(Diccionario.get('nombre', 'No existe')) # Imprime el valor asociado a la clave 'nombre' en el diccionario, si no existe, imprime 'No existe'.
+diccionario = {'nombre': 'Sandra', 'edad': 44}
+# Accediendo a los elemetos de un diccionario: 
+print(diccionario['nombre']) # Imprime el valor asociado a la clave 'nombre' en el diccionario.
+print(diccionario.get('nombre', 'No existe')) # Imprime el valor asociado a la clave 'nombre' en el diccionario, si no existe, imprime 'No existe'.
 # Get: se utiliza para acceder a los valores de un diccionario 
